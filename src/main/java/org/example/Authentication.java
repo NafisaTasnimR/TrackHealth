3a3e40c4224b6288584ea6c1ed9473308ac8b5fe
nafisa <nafisatasnim8855@gmail.com>
2025-01-25 21:03:41 +0600

    Add necessary fields and methods in Authentication class and  create test class for Authentication class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/Authentication.java
+++ src/main/java/org/example/Authentication.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Authentication {
+    private HashMap<String, User> users = new HashMap<>();
+    public boolean register(String userId,String name,int age,
+                            String gender,String email,String password)
+    {
+        return true;
+    }
+
+    public boolean login(String email,String password)
+    {
+        return true;
+    }
 }
