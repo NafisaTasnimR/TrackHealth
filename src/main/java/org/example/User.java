428567a52a6ad84ab5666d7e46d2f7810bb5cbba
nafisa <nafisatasnim8855@gmail.com>
2025-01-25 12:35:06 +0600

    Add necessary fields,constructor to User class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,43 @@
 package org.example;
 
 public class User {
+    private String userId;
+    private String userName;
+    private int age;
+    private String gender;
+    private String email;
+    private String password;
+
+    public User(String userId, String userName, int age, String gender, String email, String password) {
+        this.userId = userId;
+        this.userName = userName;
+        this.age = age;
+        this.gender = gender;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
