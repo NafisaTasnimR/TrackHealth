ccaccbcada9ecb89d10e0673f2b88a41556c93d4
nafisa <nafisatasnim8855@gmail.com>
2025-01-30 13:16:47 +0600

    Implement abstract methods and constructor in SportsTrainingGoal class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/SportsTrainingGoal.java
+++ src/main/java/org/example/SportsTrainingGoal.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class SportsTrainingGoal extends Goal{
+    public SportsTrainingGoal(double currentWeight, double targetWeight,
+                              double height, int durationInWeek, String exercisePlace) {
+        super(currentWeight, targetWeight, height, durationInWeek, exercisePlace);
+    }
+
+    @Override
+    public String setDietPlan() {
+        return null;
+    }
+
+    @Override
+    public String setWorkoutPlan() {
+        return null;
+    }
+
+    @Override
+    public String trackProgress() {
+        return null;
+    }
 }
