6d09498adf28d8573d2d5e1a996d04b9b23949c9
nafisa <nafisatasnim8855@gmail.com>
2025-01-30 12:42:04 +0600

    Implement constructor and abstract methods in WeightLossGoal class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/WeightLossGoal.java
+++ src/main/java/org/example/WeightLossGoal.java
@@ -1,4 +1,27 @@
 package org.example;
 
-public class WeightLossGoal {
+public class WeightLossGoal extends Goal{
+    public WeightLossGoal(double currentWeight, double targetWeight,
+                          double height, int durationInWeek,
+                          String exercisePlace) {
+        super(currentWeight, targetWeight, height,
+                durationInWeek, exercisePlace);
+    }
+
+    @Override
+    public String setDietPlan() {
+        return null;
+    }
+
+    @Override
+    public String setWorkoutPlan() {
+        return null;
+    }
+
+    @Override
+    public String trackProgress() {
+        return null;
+    }
+
+
 }
