17feb632eda7d21ed2caef4cec1b225da2b84273
nafisa <nafisatasnim8855@gmail.com>
2025-02-06 01:02:38 +0600

    Rename SportsTrainingGoal class to WeightMaintenanceGoal
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

---
+++ src/main/java/org/example/WeightMaintenanceGoal.java
@@ -0,0 +1,90 @@
+package org.example;
+
+import java.util.Objects;
+
+public class WeightMaintenanceGoal extends Goal implements WeightChangeCalculator,BMRCalculator,CalorieCalculator{
+    public WeightMaintenanceGoal(double currentWeight, double targetWeight,
+                                 double height, int durationInWeek, String exercisePlace) {
+        super(currentWeight, targetWeight, height, durationInWeek, exercisePlace);
+    }
+
+    @Override
+    public double calculateBMR(double weightInKg, double heightIncm, int age, String gender) {
+        double BMRValue;
+        if(Objects.equals(gender, "male"))
+        {
+            BMRValue = 88.362 + (13.397*weightInKg) + (4.799*heightIncm) - (5.677*age);
+        }
+        else {
+            BMRValue = 447.593 + (9.247*weightInKg) + (3.098*heightIncm) - (4.330*age);
+        }
+        return BMRValue;
+    }
+
+    @Override
+    public double calculateTDEE(double BMR, String activityLevel) {
+        double tdee,activityFactor;
+        if(Objects.equals(activityLevel,"Sedentary"))
+        {
+            activityFactor = 1.2;
+        } else if (Objects.equals(activityLevel,"Light Activity")) {
+            activityFactor = 1.375;
+        } else if (Objects.equals(activityLevel,"Moderate Activity")) {
+            activityFactor = 1.55;
+        } else if (Objects.equals(activityLevel,"Very Active")) {
+            activityFactor = 1.725;
+        } else if (Objects.equals(activityLevel,"Super Active")) {
+            activityFactor = 1.9;
+        }else {
+            System.out.println("Choose correct activity level");
+            activityFactor = 0.0;
+        }
+        tdee = BMR*activityFactor;
+        return tdee;
+    }
+
+    @Override
+    public double calculateDailyCalorieNeed(double TDEE) {
+        return 0;
+    }
+
+    @Override
+    public double calculateCalorieForBreakfast(double dailyCalorieNeed) {
+        return 0;
+    }
+
+    @Override
+    public double calculateCalorieForLunch(double dailyCalorieNeed) {
+        return 0;
+    }
+
+    @Override
+    public double calculateCalorieForDinner(double dailyCalorieNeed) {
+        return 0;
+    }
+
+    @Override
+    public double calculateCalorieForSnack(double dailyCalorieNeed) {
+        return 0;
+    }
+
+    @Override
+    public double calculateWeeklyWeightChange() {
+        return 0;
+    }
+
+    @Override
+    public boolean setDietPlan(String gender,int age,String activityLevel) {
+        return true;
+    }
+
+    @Override
+    public String setWorkoutPlan() {
+        return null;
+    }
+
+    @Override
+    public String trackProgress() {
+        return null;
+    }
+}
