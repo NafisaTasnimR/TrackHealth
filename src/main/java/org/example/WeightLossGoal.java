57adfca58b993bc11d4649cb20c8ef345939fa56
nafisa <nafisatasnim8855@gmail.com>
2025-02-06 13:47:49 +0600

    Implement setDietPlan method in WeightLossGoal class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/WeightLossGoal.java
+++ src/main/java/org/example/WeightLossGoal.java
@@ -85,7 +85,55 @@ public class WeightLossGoal extends Goal implements WeightChangeCalculator,BMRCa
     }
     @Override
     public boolean setDietPlan(String gender,int age,String activityLevel) {
-        return true;
+        double bmr = calculateBMR(getCurrentWeight(), getHeight(), age, gender);
+        double tdee = calculateTDEE(bmr, activityLevel);
+        double dailyCalorieNeed = calculateDailyCalorieNeed(tdee);
+        double breakfastCalories = calculateCalorieForBreakfast(dailyCalorieNeed);
+        double lunchCalories = calculateCalorieForLunch(dailyCalorieNeed);
+        double dinnerCalories = calculateCalorieForDinner(dailyCalorieNeed);
+        double snackCalories = calculateCalorieForSnack(dailyCalorieNeed);
+        if(tdee != 0.00) {
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            System.out.printf("| %-94s |\n","Your Weekly Weight Loss Target Is " + calculateWeeklyWeightChange() + "kg");
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            System.out.printf("| %-20s | %-60s | %-8s |\n", "Meal", "Food Items", "Calories");
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            System.out.printf("| %-20s | %-60s | %-8.2f |\n", "Morning",
+                    "Eggs, Whole Grain Bread, Oats, Rice Porridge, Yogurt,", breakfastCalories);
+            System.out.printf("| %-20s | %-60s | %-8s |\n", "",
+                    "Fruits, Avocado, Nuts (Almonds, Walnuts), Coffee, Tea", "");
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            System.out.printf("| %-20s | %-60s | %-8.2f |\n", "Mid-Morning Snack",
+                    "Greek Yogurt, Nuts, Fruits (Berries, Orange, Banana),", snackCalories);
+            System.out.printf("| %-20s | %-60s | %-8s |\n", "",
+                    "Dark Chocolate(At least 70% cocoa), Cottage Cheese","");
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            System.out.printf("| %-20s | %-60s | %-8.2f |\n", "Lunch",
+                    "Brown Rice, Whole Wheat Bread, Fish, Lentils,",lunchCalories);
+            System.out.printf("| %-20s | %-60s | %-8s |\n", "",
+                    "Grilled Chicken, Steamed Veggies, Salad, Olive Oil","");
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            System.out.printf("| %-20s | %-60s | %-8.2f |\n", "Afternoon Snack",
+                    "Boiled Egg, Trail Mix, Low-Fat Cheese,",snackCalories);
+            System.out.printf("| %-20s | %-60s | %-8s |\n", "",
+                    "Fruit Salad, Protein Bar","");
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            System.out.printf("| %-20s | %-60s | %-8.2f |\n", "Dinner",
+                    "Rice or Bread, Vegetables, Soup, Tofu, Beans,",dinnerCalories);
+            System.out.printf("| %-20s | %-60s | %-8s |\n", "",
+                    "Grilled Fish or Chicken, Stir-Fried Veggies with Olive Oil","");
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            return true;
+        }
+        return false;
     }
 
     @Override
--- src/test/java/GoalTest.java
+++ src/test/java/GoalTest.java
@@ -1,15 +1,26 @@
 import org.example.Goal;
 import org.example.WeightGainGoal;
+import org.example.WeightLossGoal;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class GoalTest {
     @Test
-    public void testSetGoal()
+    public void testSetGoalForWeightGain()
     {
         boolean expectedValue = true;
         Goal weightGain = new WeightGainGoal(61.90,57.00,
+                155.58,12,"home");
+        boolean actualValue = weightGain.setDietPlan("female",21,"Light Activity");
+        assertEquals(expectedValue,actualValue);
+    }
+
+    @Test
+    public void testSetGoalForWeightLoss()
+    {
+        boolean expectedValue = true;
+        Goal weightGain = new WeightLossGoal(61.90,57.00,
                 155.58,12,"home");
         boolean actualValue = weightGain.setDietPlan("female",21,"Light Activity");
         assertEquals(expectedValue,actualValue);
