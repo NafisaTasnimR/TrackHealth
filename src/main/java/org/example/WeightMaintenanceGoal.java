712d3babcd6b473b07fbcc22230c8dd895cb2def
nafisa <nafisatasnim8855@gmail.com>
2025-02-06 22:07:13 +0600

    Implement setDietPlan method in WeightMaintenanceGoal class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/WeightMaintenanceGoal.java
+++ src/main/java/org/example/WeightMaintenanceGoal.java
@@ -9,6 +9,11 @@ public class WeightMaintenanceGoal extends Goal implements WeightChangeCalculato
     }
 
     @Override
+    public double calculateWeeklyWeightChange() {
+        return 0;
+    }
+
+    @Override
     public double calculateBMR(double weightInKg, double heightIncm, int age, String gender) {
         double BMRValue;
         if(Objects.equals(gender, "male"))
@@ -23,7 +28,7 @@ public class WeightMaintenanceGoal extends Goal implements WeightChangeCalculato
 
     @Override
     public double calculateTDEE(double BMR, String activityLevel) {
-        double tdee,activityFactor;
+        double activityFactor;
         if(Objects.equals(activityLevel,"Sedentary"))
         {
             activityFactor = 1.2;
@@ -39,43 +44,86 @@ public class WeightMaintenanceGoal extends Goal implements WeightChangeCalculato
             System.out.println("Choose correct activity level");
             activityFactor = 0.0;
         }
-        tdee = BMR*activityFactor;
-        return tdee;
+        return BMR*activityFactor;
     }
 
     @Override
     public double calculateDailyCalorieNeed(double TDEE) {
-        return 0;
+        return TDEE;
     }
 
     @Override
     public double calculateCalorieForBreakfast(double dailyCalorieNeed) {
-        return 0;
+        return dailyCalorieNeed*0.2;
     }
 
     @Override
     public double calculateCalorieForLunch(double dailyCalorieNeed) {
-        return 0;
+        return dailyCalorieNeed*0.3;
     }
 
     @Override
     public double calculateCalorieForDinner(double dailyCalorieNeed) {
-        return 0;
+        return dailyCalorieNeed*0.3;
     }
 
     @Override
     public double calculateCalorieForSnack(double dailyCalorieNeed) {
-        return 0;
-    }
-
-    @Override
-    public double calculateWeeklyWeightChange() {
-        return 0;
+        return dailyCalorieNeed*0.1;
     }
 
     @Override
     public boolean setDietPlan(String gender,int age,String activityLevel) {
-        return true;
+        double bmr = calculateBMR(getCurrentWeight(), getHeight(), age, gender);
+        double tdee = calculateTDEE(bmr, activityLevel);
+        double dailyCalorieNeed = calculateDailyCalorieNeed(tdee);
+        double breakfastCalories = calculateCalorieForBreakfast(dailyCalorieNeed);
+        double lunchCalories = calculateCalorieForLunch(dailyCalorieNeed);
+        double dinnerCalories = calculateCalorieForDinner(dailyCalorieNeed);
+        double snackCalories = calculateCalorieForSnack(dailyCalorieNeed);
+        if(tdee != 0.00) {
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            System.out.printf("| %-94s |\n","You Are Maintaining Your Current Weight");
+            System.out.printf("| %-94s |\n","Tips: Avoid Overeating & Stick to your TDEE");
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            System.out.printf("| %-20s | %-60s | %-8s |\n", "Meal", "Food Items", "Calories");
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            System.out.printf("| %-20s | %-60s | %-8.2f |\n", "Morning",
+                    "Eggs, Whole Grain Bread, Oats, Rice Porridge,", breakfastCalories);
+            System.out.printf("| %-20s | %-60s | %-8s |\n", "",
+                    "Yogurt, Coffee, Tea, Milk", "");
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            System.out.printf("| %-20s | %-60s | %-8.2f |\n", "Mid-Morning Snack",
+                    "Greek Yogurt, Nuts,", snackCalories);
+            System.out.printf("| %-20s | %-60s | %-8s |\n", "",
+                    " Fruits (Berries, Orange, Banana)","");
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            System.out.printf("| %-20s | %-60s | %-8.2f |\n", "Lunch",
+                    "Brown Rice, Whole Wheat Bread, Fish, Lentils,",lunchCalories);
+            System.out.printf("| %-20s | %-60s | %-8s |\n", "",
+                    "Chicken, Veggies, Salad","");
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            System.out.printf("| %-20s | %-60s | %-8.2f |\n", "Afternoon Snack",
+                    "Boiled Egg, Trail Mix, Milkshake",snackCalories);
+            System.out.printf("| %-20s | %-60s | %-8s |\n", "",
+                    "Fruit Salad, Protein Bar","");
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            System.out.printf("| %-20s | %-60s | %-8.2f |\n", "Dinner",
+                    "Rice or Bread, Vegetables, Soup, Potatoes, Beans,",dinnerCalories);
+            System.out.printf("| %-20s | %-60s | %-8s |\n", "",
+                    " Fish or Chicken, Vegetables","");
+            System.out.println("----------------------------------------------------------------------------------" +
+                    "----------------");
+            return true;
+        }
+        return false;
     }
 
     @Override
