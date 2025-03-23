00d0d776dff8c89d3b280002bd81fb8e0eb51639
nafisa <nafisatasnim8855@gmail.com>
2025-03-24 01:59:06 +0600

    Add new method setWorkOutPlan in Goal class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/Goal.java
+++ src/main/java/org/example/Goal.java
@@ -14,14 +14,14 @@ public abstract class Goal {
     }
 
     public boolean setDietPlan(String gender, int age, String activityLevel) {
-        double bmi = HealthMetricsCalculator.calculateBMI(currentWeight, height);
+        double bmi = HealthMetricsCalculator.calculateBMI(getCurrentWeight(), getHeight());
         System.out.println("----------------------------------------------------------------------------------" +
                 "----------------");
         System.out.printf("| %-10s %-5.2f %-78s|\n","Your BMI: " , bmi , " (" + HealthMetricsCalculator.getWeightCategory(bmi) + ")");
         System.out.println("----------------------------------------------------------------------------------" +
                 "----------------");
 
-        double bmr = HealthMetricsCalculator.calculateBMR(currentWeight, height, age, gender);
+        double bmr = HealthMetricsCalculator.calculateBMR(getCurrentWeight(), getHeight(), age, gender);
         double tdee = HealthMetricsCalculator.calculateTDEE(bmr, activityLevel);
         double dailyCalorieNeed = adjustCalories(tdee);
 
@@ -30,7 +30,15 @@ public abstract class Goal {
 
         return true;
     }
-    public abstract String setWorkoutPlan();
+    public boolean setWorkoutPlan(){
+        WorkoutPlanService workoutPlan = createWorkoutPlanService();
+        if(workoutPlan == null){
+            return false;
+        }
+        workoutPlan.suggestWorkoutPlan();
+        return true;
+    }
+
     protected abstract double adjustCalories(double tdee);
     protected abstract MealPlanService createMealPlanService();
     protected abstract WorkoutPlanService createWorkoutPlanService();
