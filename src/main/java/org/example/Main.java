672659ad31b174cfa2d74c5377e68e4e18693b10
nafisa <nafisatasnim8855@gmail.com>
2025-03-24 15:46:21 +0600

    Implement initial user interface for login
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,61 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+    private static boolean isRunning = true;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        mainMenu();
+    }
+    public static void mainMenu()
+    {
+        Scanner scanner = new Scanner(System.in);
+        while(isRunning) {
+            System.out.println(" ".repeat(50) + "-----------------------TrackHealth-----------------------");
+            System.out.println(" ".repeat(75) + "1. Login");
+            System.out.println(" ".repeat(75) + "2. Sign Up");
+            System.out.println(" ".repeat(75) + "3. Exit");
+            System.out.print(" ".repeat(75) + "Enter Your Choice: ");
+            int caseValue = scanner.nextInt();
+
+            switch (caseValue) {
+                case 1 -> {
+                    System.out.println("//////////////////////////////////////////////");
+                    loginMenu(scanner);
+                }
+                case 2 -> {
+                    System.out.println("//////////////////////////////////////////////");
+                    registrationMenu(scanner);
+                }
+                case 3 -> exitApplication();
+                default -> System.out.println("Invalid selection. Try again.");
+            }
+        }
+
+    }
+    private static void exitApplication() {
+        System.out.println("Thank You For Using HireUp!");
+        System.out.println("Exiting The System...");
+        isRunning = false;
+        System.exit(0);
+    }
+
+    private static void loginMenu(Scanner scanner) {
+        scanner.nextLine();
+        System.out.print(" ".repeat(75) + "Enter Your Email: ");
+        String email = scanner.nextLine();
+        System.out.println();
+        System.out.print(" ".repeat(75) + "Enter Your Password: ");
+        String password = scanner.nextLine();
+        System.out.println();
+        UserRepository userRepository = new CSVUserRepository();
+        AuthenticationService authenticationService = new AuthenticationService(userRepository);
+        if(authenticationService.login(email,password)) {
+            System.out.println(" ".repeat(75) + "Welcome To TrackHealth!!");
+        }
+    }
+
+    private static void registrationMenu(Scanner scanner) {
+
     }
 }
