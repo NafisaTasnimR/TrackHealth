f39a712618b8486dc771bf96ac0664b722a4993d
nafisa <nafisatasnim8855@gmail.com>
2025-03-24 13:09:28 +0600

    Implement necessary fields and method in Progress class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/Progress.java
+++ src/main/java/org/example/Progress.java
@@ -1,4 +1,39 @@
 package org.example;
 
 public class Progress {
+    private double initialWeight;
+    private double targetWeight;
+    private double currentWeight;
+    private int goalDurationDays;
+
+    public Progress(double initialWeight, double targetWeight, double currentWeight, int goalDurationDays) {
+        this.initialWeight = initialWeight;
+        this.targetWeight = targetWeight;
+        this.currentWeight = currentWeight;
+        this.goalDurationDays = goalDurationDays;
+    }
+
+    public double getInitialWeight() {
+        return initialWeight;
+    }
+
+    public double getTargetWeight() {
+        return targetWeight;
+    }
+
+    public double getCurrentWeight() {
+        return currentWeight;
+    }
+
+    public int getGoalDurationDays() {
+        return goalDurationDays;
+    }
+    public double calculateProgressPercentage() {
+        if (initialWeight == targetWeight) {
+            return 100.0;
+        }
+        double progress = 100 * Math.abs(currentWeight - initialWeight) / Math.abs(targetWeight - initialWeight);
+        return Math.min(progress, 100.0);
+    }
+    
 }
