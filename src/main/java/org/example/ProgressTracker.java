2187db233ec8a17ba150c02584c7079fcc4f7a5d
nafisa <nafisatasnim8855@gmail.com>
2025-03-24 14:33:52 +0600

    Add necessary fields and methods in ProgressTracker class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/ProgressTracker.java
+++ src/main/java/org/example/ProgressTracker.java
@@ -1,5 +1,22 @@
 package org.example;
 
 public class ProgressTracker {
-    
+    private Progress progress;
+
+    public Progress getProgress() {
+        return progress;
+    }
+
+    public ProgressTracker(Progress progress) {
+        this.progress = progress;
+    }
+
+    public void logProgress(double currentWeight) {
+        getProgress().updateCurrentWeight(currentWeight);
+        System.out.printf("Progress: %.2f%%\n", getProgress().calculateProgressPercentage());
+    }
+    public boolean isGoalAchieved() {
+        return progress.calculateProgressPercentage() >= 100.0;
+    }
+
 }
