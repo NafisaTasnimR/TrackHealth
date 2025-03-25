9c3a21426b919ffd38a26e25cb64666c989cb3eb
nafisa <nafisatasnim8855@gmail.com>
2025-03-25 17:18:33 +0600

    Add new method showGoalReminder in NotificationService class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/NotificationService.java
+++ src/main/java/org/example/NotificationService.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class NotificationService {
+    public void showGoalReminder(String email, GoalFileHandler goalTracker) {
+        if (goalTracker.getGoalData(email) != null) {
+            System.out.println("Reminder: Don't forget to log your weight today!");
+        }
+    }
 }
