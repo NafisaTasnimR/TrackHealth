b5b17f6af0637add47707813606fbd03ebba4be5
nafisa <nafisatasnim8855@gmail.com>
2025-03-26 21:26:23 +0600

    Remove Progress as a field from ProgressTracker class and 	fix associated methods
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/ProgressTracker.java
+++ src/main/java/org/example/ProgressTracker.java
@@ -3,38 +3,32 @@ package org.example;
 import java.util.List;
 
 public class ProgressTracker {
-    private Progress progress;
     private TrackerFileHandler fileHandler;
 
-    public ProgressTracker(Progress progress) {
-        this.progress = progress;
+    public ProgressTracker() {
         this.fileHandler = new TrackerFileHandler("weight_log.csv");
     }
 
-    public Progress getProgress() {
-        return progress;
-    }
-
-    public boolean logCurrentWeight(double weight,String email) {
-        if(progress != null) {
-            getProgress().updateCurrentWeight(weight);
+    public boolean logCurrentWeight(Progress progress, double weight, String email) {
+        if (progress != null) {
+            progress.updateCurrentWeight(weight);
             fileHandler.saveWeight(email, weight);
-            displayProgress(email);
+            displayProgress(progress, email);
             return true;
-        }else {
+        } else {
             System.out.println("You Haven't Entered Any Of The Necessary Information!");
         }
         return false;
     }
 
-    public void displayProgress(String email) {
-        double progressPercentage = getProgress().calculateProgressPercentage();
-        double distanceToGoal = getProgress().getTargetWeight() - getProgress().getCurrentWeight();
+    public void displayProgress(Progress progress, String email) {
+        double progressPercentage = progress.calculateProgressPercentage();
+        double distanceToGoal = progress.getTargetWeight() - progress.getCurrentWeight();
 
         System.out.println("User: " + email);
-        System.out.println("Initial weight: " + getProgress().getInitialWeight());
-        System.out.println("Current weight: " + getProgress().getCurrentWeight());
-        System.out.println("Target weight: " + getProgress().getTargetWeight());
+        System.out.println("Initial weight: " + progress.getInitialWeight());
+        System.out.println("Current weight: " + progress.getCurrentWeight());
+        System.out.println("Target weight: " + progress.getTargetWeight());
         System.out.println("Progress: " + String.format("%.2f", progressPercentage) + "%");
 
         if (distanceToGoal > 0) {
@@ -51,16 +45,15 @@ public class ProgressTracker {
         System.out.println("\nWeight History for " + email + ":");
         if (history.isEmpty()) {
             System.out.println("No records found.");
-        } else {
-            for (String entry : history) {
-                System.out.println(entry);
-            }
-            return true;
+            return false;
         }
-        return false;
-    }
-    public boolean isGoalAchieved() {
-        return getProgress().calculateProgressPercentage() >= 100.0;
+        for (String entry : history) {
+            System.out.println(entry);
+        }
+        return true;
     }
 
+    public boolean isGoalAchieved(Progress progress) {
+        return progress.calculateProgressPercentage() >= 100.0;
+    }
 }
