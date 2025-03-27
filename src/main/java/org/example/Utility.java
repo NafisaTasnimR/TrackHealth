b9df10246c89133ca42935cb0098cca033a62395
nafisa <nafisatasnim8855@gmail.com>
2025-03-27 06:57:23 +0600

    Implement generateId in Utility class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/Utility.java
+++ src/main/java/org/example/Utility.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Utility {
+    private static int idCounter = 1;
+    public static String generateId() {
+        String formattedId = String.format("user%02d", idCounter);
+        idCounter++;
+        return formattedId;
+    }
 }
