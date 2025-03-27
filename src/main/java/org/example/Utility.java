caf7b0b1994ee8176e7641e7da3783fe211caf74
nafisa <nafisatasnim8855@gmail.com>
2025-03-27 07:31:15 +0600

    Implement isValidEmail method in Utility class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/Utility.java
+++ src/main/java/org/example/Utility.java
@@ -1,10 +1,19 @@
 package org.example;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Utility {
     private static int idCounter = 1;
     public static String generateId() {
         String formattedId = String.format("user%02d", idCounter);
         idCounter++;
         return formattedId;
+    }
+    public static boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
