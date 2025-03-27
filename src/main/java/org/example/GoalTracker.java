a164b89fbb6e6c78f117f61d941cb64a8a450d42
nafisa <nafisatasnim8855@gmail.com>
2025-03-27 13:51:22 +0600

    Implement necessary methods in GoalTracker class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/GoalTracker.java
+++ src/main/java/org/example/GoalTracker.java
@@ -1,4 +1,34 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class GoalTracker {
+    private final GoalFileHandler goalFileHandler;
+
+    public GoalTracker(GoalFileHandler goalFileHandler) {
+        this.goalFileHandler = goalFileHandler;
+    }
+
+    public boolean hasGoal(String email) {
+        return goalFileHandler.getGoalData(email) != null;
+    }
+
+    public boolean shouldRemindToday(String email) {
+        LocalDate lastLoggedDate = goalFileHandler.getLastLoggedDate(email);
+        return lastLoggedDate == null || !lastLoggedDate.isEqual(LocalDate.now());
+    }
+
+    public boolean isGoalCompleted(String email) {
+        GoalInformation goalInfo = goalFileHandler.getGoalData(email);
+        if (goalInfo == null) return false;
+
+        LocalDate startDate = LocalDate.parse(goalInfo.getStartDate());
+        int durationDays = goalInfo.getTimeDuration();
+        LocalDate endDate = startDate.plusDays(durationDays);
+        return !LocalDate.now().isBefore(endDate);
+    }
+
+    public int calculateDaysPassed(String email) {
+        return goalFileHandler.calculateDaysPassed(email);
+    }
 }
