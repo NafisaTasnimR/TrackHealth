05e8a782072c5eeffb211562b76b812eed8f5641
nafisa <nafisatasnim8855@gmail.com>
2025-03-29 00:30:27 +0600

    Implement RewardService class with necessary methods
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/RewardService.java
+++ src/main/java/org/example/RewardService.java
@@ -1,5 +1,29 @@
 package org.example;
 
 public class RewardService {
-    
+    private GoalTracker goalTracker;
+    private Reward reward;
+
+    public RewardService() {
+        this.goalTracker = new GoalTracker();
+        this.reward = new Reward();
+    }
+
+    public GoalTracker getGoalTracker() {
+        return goalTracker;
+    }
+
+    public Reward getReward() {
+        return reward;
+    }
+    public void getRewardMessage(String email,ProgressCalculator progressCalculator) {
+        if(getGoalTracker().isGoalCompleted(email)) {
+            if(getGoalTracker().isGoalAchieved(progressCalculator)) {
+                getReward().displayCongratulatoryMessage();
+                getReward().displayAdditionalMessage();
+            } else {
+                getReward().displayMessageForGoalNotAchieved();
+            }
+        }
+    }
 }
