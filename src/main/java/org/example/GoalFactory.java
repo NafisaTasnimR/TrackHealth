20319fc96a53d7406699b6b237645fec274264c4
nafisa <nafisatasnim8855@gmail.com>
2025-04-02 14:23:15 +0600

    Add necessary method in GoalManager class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/org/example/GoalFactory.java
+++ src/main/java/org/example/GoalFactory.java
@@ -1,4 +1,20 @@
 package org.example;
 
 public class GoalFactory {
+    public Goal getGoal(String goalType,GoalInformation goalInformation){
+        if(goalType == null){
+            return null;
+        }
+        if(goalType.equalsIgnoreCase("WEIGHTGAIN")){
+            return new WeightGainGoal(goalInformation);
+
+        } else if(goalType.equalsIgnoreCase("WEIGHTLOSS")){
+            return new WeightLossGoal(goalInformation);
+
+        } else if(goalType.equalsIgnoreCase("WEIGHTMAINTENANCE")){
+            return new WeightMaintenanceGoal(goalInformation);
+        }
+
+        return null;
+    }
 }
