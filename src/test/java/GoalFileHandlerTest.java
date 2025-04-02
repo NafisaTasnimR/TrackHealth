710ce568ef21715c9271fd0764456c6c84f08531
nafisa <nafisatasnim8855@gmail.com>
2025-04-02 09:42:28 +0600

    Create new GoalFileHandlerTest class to test the methods of GoalFileHandler class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

---
+++ src/test/java/GoalFileHandlerTest.java
@@ -0,0 +1,25 @@
+import org.example.GoalFileHandler;
+import org.example.GoalInformation;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GoalFileHandlerTest {
+    @Test
+    public void testSaveData() {
+        boolean expectedValue = true;
+        GoalInformation goalInformation = new GoalInformation(62.75,57.00,
+                156.55,1,2,"gym","weightLoss",
+                "2025-04-02","tasfiarahman@gmail.com");
+        GoalFileHandler goalFileHandler = new GoalFileHandler();
+        boolean actualValue = goalFileHandler.saveGoalData(goalInformation);
+        assertEquals(expectedValue,actualValue);
+    }
+    @Test
+    public void testDeleteUserDataFromFile() {
+        boolean expectedValue = true;
+        GoalFileHandler goalFileHandler = new GoalFileHandler();
+        boolean actualValue = goalFileHandler.deletePreviousGoal("tasfiarahman@gmail.com");
+        assertEquals(expectedValue,actualValue);
+    }
+}
